2a1c221d05c68ce69b9f7e7d2d6ed7a6db732f27
sharryXheisenberg <pandesaurabh4596@gmail.com>
2025-07-20 09:03:59 +0530

    variables data types

---
+++ var_datatypes/var.js
@@ -0,0 +1,45 @@
+console.log('Hello'); 
+console.log("kaskd");   
+
+let variable
+
+let message = "Hello !";
+console.log(message);
+// Identifiers in JavaScript are case-sensitive. For example, the message is different from the Message.
+
+
+// In JS by using let keyword we can declare multiple values of different datatype to vairables
+let var1 = "Hello bro! ",
+ cont = 123;
+
+ console.log(var1);
+ console.log(cont);
+
+//  changing variable 
+
+ let msg = "Good bye!";
+ msg = "By";
+
+ // when we tries to print it 
+ console.log(msg);
+
+//  Undefined vs. undeclared variables
+// let msg;
+// console.log(msg); // undefined
+
+console.log(cnt); // error 
+
+let counter = 123;
+console.log(typeof(counter));
+
+counter = false;
+
+counter = 'Hi';
+console.log(ty)
+
+let x = 10;
+if (x == 10) {
+    let x = 20;
+    console.log(x); // 20:  reference x inside the block
+}
+console.log(x); // 10: reference at the begining of the script
