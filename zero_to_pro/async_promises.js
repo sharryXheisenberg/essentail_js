06243c900fdb1044eae0de7a4ef3790df8b42652
sharryXheisenberg <pandesaurabh4596@gmail.com>
2025-10-07 09:53:05 +0530

    Updated learning

--- zero_to_pro/async_promises.js
+++ zero_to_pro/async_promises.js
@@ -62,8 +62,160 @@ const faiCallBack = (e) => console.log(`Failure: ${e}`);
 anotherCallBack(3,sucCallBack,faiCallBack);
 
 
+// callback
+//  Callbacks are the first thing we should understand when we are talking
+//  about concurrency. The good news is that the 
+// callback principle is not too
+//  hard to understand. It is just a function that takes another function as an
+//  argument, which is then called when the rest of the initial function has
+//  finished.
+
+function doSomething(callback){
+    callback();
+}
+
+function sayHi(){
+    console.log("Hi!!");
+}
+doSomething(sayHi);
+
+
+// another example
+function judge(grade){
+    switch(true) {
+        case grade == "A":
+            console.log("You got an", grade , ":amazing!");
+            break;
+        case grade == "B":
+            console.log("You got an", grade , ":well done!");
+            break;
+        case grade == "C":
+            console.log("You got an", grade , ":ok!");
+            break;
+        case grade == "D":
+            console.log("You got an", grade , ":hmm....!");
+            break;
+        case grade == "F":
+            console.log("You got an", grade , ":Bro you are failed!");
+        default:
+            console.log("Write correct grade or you are an absent?");
+    }
+}
+function getGrade(score,callback){
+    let grade;
+    switch(true){
+        case score >=90:
+            grade = "A";
+            break;
+
+        case score >=80:
+            grade = "B";
+            break;
+
+        case score >=70:
+            grade = "C";
+            break;
+
+        case score >=60:
+            grade = "D";
+            break;
+
+        default:
+            grade = "F";
+    }
+     callback(grade);
+
+};
+
+getGrade(85,judge);
+
+
+
+//  setTimeOut() and setInterval() 
+
+// setInterval(encourage,500);
+
+// function encourage(){
+//     console.log("You are bro! (it is called after 500ms)");
+// }
+
+
+// exercise
+function greet(fullName){
+    console.log(`Hi! , ${fullName[0]} ${fullName[1]}`);
+}
+
+function getUserName(userFullName , callback){
+    let name = userFullName.split(" ");
+    callback(name); // why this callback is used 
+}
+getUserName("Saurabh Pande",greet);
 
 // PROMISES
+//  A Promise is a special object that connects codethat needs to produce a result and the code that needs to use this result in the next step
+
+//  
+// resolve or 
+// res
+//  resolve() is called, the Promise is presumed to be successful and
+//  whatever is between the arrows is returned and used as input for the 
+// then
+//  method on the 
+// Promise object. 
+
+//  If reject() is called, the and the catch() method on the Promise failed
+//  Promise object (if present) is executed
+//  with the argument of the 
+// reject() function.
+
+let promise = new Promise(function(resolve,reject){
+    let x = 20;
+
+    if(x>10){
+        resolve(x);
+    }else{
+        reject(`Too low ${x}`);
+    }
+});
+
+promise.then(
+    function (value){
+        console.log("Success:",value);
+    },
+    function (error){
+        console.log("Error:",value);
+    }
+);
+
+//  then() is a Promise itself, so when it returns we can use the result for the next then() instance. This means we can chain the then() instances,
+
+const prom = new Promise((resolve,reject)=>{
+    resolve("Success");
+    reject("Opps...");
+})
+.then(value=>{
+    console.log(value);
+    return "we";
+})
+.then(value=>{
+    console.log(value);
+    return "are";
+})
+.then(value=>{
+    console.log(value);
+    return "seeing";
+})
+.then(value=>{
+    console.log(value);
+    return "Promise chaining";
+})
+.then(value=>{
+    console.log(value);
+})
+.catch(value=>{
+    console.log(value);
+})
+
 function fetchData(){
     return new Promise((resolve ,reject)=>{
         setTimeout(()=>{
